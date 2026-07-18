4119212c9ce02bda62815223f450d5aa1a091cc3
Russell Oje <russelloje@gmail.com>
2026-07-18 10:33:43 +0100

    fix: create deploy script for new frontend app

--- my-frontend/vite.config.ts
+++ my-frontend/vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/solana-100-days-mlh/my-frontend/',
   plugins: [react(), tailwindcss()],
 });
